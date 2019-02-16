b6ec1dccb8ed9c17c61736f15c4cb434c971f72b
Dylan Heinrich <harly13@live.com>
2019-02-16 19:40:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 function takeANumber(deliLine, newName) {
-  deliLine.push(newName)
-  return `Welcome, ${newName}. You are number ${deliLine.length} in line.`
+  deliLine.push(newName);
+  return `Welcome, ${newName}. You are number ${deliLine.length} in line.`;
 }
 
 function nowServing(deliLine) {
   if(deliLine.length > 0){
-    var firstInLine = deliLine[0]
-    deliLine.shift()
-    return `Currently serving ${firstInLine}.`
+    var firstInLine = deliLine[0];
+    deliLine.shift();
+    return `Currently serving ${firstInLine}.`;
   }
   else {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
 }
 
 function currentLine(deliLine) {
-  var line = "The line is currently: "
+  var line = "The line is currently: ";
   if(deliLine.length > 0){
     for(var i = 0; i < deliLine.length; i++){
-      line += `${i+1}. ${deliLine[i]}`
+      line += `${i+1}. ${deliLine[i]}`;
     }
-    return line
+    return line;
   }
   else {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
 }
