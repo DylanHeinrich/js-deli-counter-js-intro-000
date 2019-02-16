79bf8c1d6ca5632e895d85ca656e4bfeeae31384
Dylan Heinrich <harly13@live.com>
2019-02-16 18:18:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(deliLine, newName) {
   deliLine.push(newName)
-  return `Welcome ${newName}. You are number ${deliLine.lenght + 1} in line.`
+  return `Welcome ${newName}. You are number ${deliLine.length + 1} in line.`
 }
