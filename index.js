ae051e89a4b88804e12445d34e31610bb1a19b2f
Dylan Heinrich <harly13@live.com>
2019-02-16 18:22:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function takeANumber(deliLine, newName) {
   deliLine.push(newName)
   return `Welcome, ${newName}. You are number ${deliLine.length} in line.`
 }
+
+function nowServing(deliLine) {
+  if(deliLine.length > 0){
+    var firstInLine = deliLine[0]
+    delete deliLine[0]
+    return firstInLine
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
